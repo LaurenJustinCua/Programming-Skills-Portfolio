c777a7b0c09d4fa532230c2483eaded73d23f53a
LaurenJustinCua <144773686+LaurenJustinCua@users.noreply.github.com>
2023-10-01 14:03:29 +0400

    Add files via upload

---
+++ Chapter 1/Exercises/chap-1ex-1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 11:01:10 2023
+
+@author: Lauren Justin
+"""
+
+"""
+Chapter 1 Exercise 1: Print Strings
+"""
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are !\n\t\tUp above the world so high,\n\t\tlike a diamon in the sky.\nTwinkle, twinkle, little star,\n\t\How I wonder what you are,")
---
+++ Chapter 1/Exercises/chap-1ex-2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 11:40:25 2023
+
+@author: Lauren Justin
+"""
+
+"""
+Chpater 1, Excercise 2: Print the Version of Python
+"""
+
+print("""He said","javascript is awesome"\nI replied, \n python is more awesome""")
